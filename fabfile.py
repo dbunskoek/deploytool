b11c2ae5fa6bf6bb159ed442152b77decb450036
Nick Badoux <nbadoux@leukeleu.nl>
2012-01-13 16:12:58 +0100

    renamed project-settings

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,11 @@
 import hosts.staging.tasks as staging
 import sys; sys.path.append('..')
-import settings_default
+import settings
 
 
 # django project settings
-PROJECT_SETTINGS = getattr(settings_default, 'DEPLOYMENT')
+PROJECT_SETTINGS = getattr(settings, 'DEPLOYMENT')
+sys.path.remove('..')
 
 # available tasks
 staging.deploy = staging.Deployment(project_settings=PROJECT_SETTINGS)
